8cdb4ef87de9c61d2923a37a36c857e06c81c5fe
Fabian Schuetze <fabian.schutze@eui.eu>
2020-08-05 13:38:49 +0200

    local files

--- cpp_client/tests/initialize.cpp
+++ cpp_client/tests/initialize.cpp
@@ -15,8 +15,8 @@
 #include "../TS_SSDLiteCaller.hpp"
 
 TEST(INIT, LoadConfig) {
-    std::string root = "/home/fabian/Documents/work/github/pytorch-ssd/";
-    std::string model = root + "traced_quantized.pt";
+    std::string root = "C:\\Users\\fs\\pytorch-ssd\\";
+    std::string model = root + "traced_ssdlite.pt";
     std::string wrong_location = root + "params.txt";
     ASSERT_THROW(TS_SSDLiteCaller SSDLite(model, wrong_location),
                  std::runtime_error);
@@ -25,21 +25,19 @@ TEST(INIT, LoadConfig) {
 }
 
 TEST(INIT, LoadModel) {
-    std::string root = "/home/fabian/Documents/work/github/pytorch-ssd/";
+    std::string root = "C:\\Users\\fs\\pytorch-ssd\\";
     std::string wrong_model = root + "does_not_exists.pt";
     std::string location = root + "cpp_client/params.txt";
     ASSERT_ANY_THROW(TS_SSDLiteCaller SSDLite(wrong_model, location));
                  //std::runtime_error);
-    std::string model = root + "traced_quantized.pt";
+    std::string model = root + "traced_ssdlite.pt";
     ASSERT_NO_THROW(TS_SSDLiteCaller SSDLite(model, location));
 }
 
 TEST(INIT, LoadDataBase) {
     std::string wrong_location = "/home/fabian/test";
     ASSERT_THROW(Database database(wrong_location), std::runtime_error);
-    std::string correct_file =
-        "/home/fabian/data/TS/CrossCalibration/TCLObjectDetectionDatabase/"
-        "database.xml";
+    std::string correct_file = "C:\\Users\\fs\\Documents\\CrossCalibration\\TCLObjectDetectionDatabase\\greyscale.xml";
     ASSERT_NO_THROW(Database database(correct_file));
 }
 
